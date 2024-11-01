b6007eb8089a15ea593aedd3d5ff7b25b7c58a2b
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-01 16:03:21 +0600

    practice rest and spread operator

--- module1/src/1.6.ts
+++ module1/src/1.6.ts
@@ -1,5 +1,5 @@
 //Learning function
-
+{
 function add(num1: number,num2:number=10):number{
     return num1+num2;
 }
@@ -15,4 +15,8 @@ const poorUser={
     addBalance(balance:number):string{
         return `my new balace is : ${this.banlace+balance}`
     }
+}
+//array map
+const arr: number[]=[1,4,10];
+const newArr : number[] = arr.map((elemnt:number):number=>elemnt*elemnt);
 }
---
+++ module1/src/1.7.ts
@@ -0,0 +1,36 @@
+{
+ 
+
+const poorUser={
+    name:"mr x",
+}
+
+   //spread operator
+   
+    //destructuring
+
+const bros1 : string[]=['mir','firoz','mizan' ]
+const bros2 : string[]=['tonmoy','nahid','rahat']
+
+bros1.push(...bros2);   
+}
+const mentors1={
+    typescript:'mezba',
+    redux:'Mir',
+    dbms:'Mizan'
+}
+const mentor2={
+    prisma:'Firoz',
+    next:'tonmoy',
+    cloud:'Nahid'
+}
+const mentorList ={
+    ...mentors1,
+    ...mentor2
+}
+ //rest operator
+ const greetFriends=(...friends:string[])=>{
+    
+    friends.forEach((friend:string)=>console.log(`Hi ${friend1} ${friend2} ${friend3}`);)
+ }
+ greetFriends("Abul","kabul","babul","ubul")
