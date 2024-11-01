7f3fc848b7812e94b22656c42353bd3cd1b080a0
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-01 14:34:13 +0600

    practecing typescript

---
+++ module1/src/1.5.ts
@@ -0,0 +1,12 @@
+const user:{
+    university:"Programming Hero"; //we can't change this name,we have to input university name as programming hero.by default programming hero is outr data type
+    firstName:string;
+    middleName?:string;//optional type
+    lastName:string;
+    isMarried:boolean;
+} ={
+    firstName:"MD.Rayhan",
+    lastName:"Shorker",
+    isMarried:false,
+    university:"Programming Hero"
+}
