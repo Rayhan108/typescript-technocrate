f316ad2712a5044ae4dec2d6288afc53353e43ed
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-06 08:13:24 +0600

    practicing nullisg coalecing

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -1,5 +1,45 @@
 {
 
+const age:number=18;
+if(age>=18){
+    console.log('adult');
+}else{
+    console.log('teneger');
+}
+const isAdult = age>18?'adult':'not adult';
+console.log(isAdult);
+
+//nullish coalessing
+const isAuthenticate = null;
+const result = isAuthenticate??'Guest';
+console.log(result);
+type User ={
+    name:string;
+    address:{
+        city:string;
+        road:string;
+        presentAddress:string;
+        permenentAddress?:string;
+
+    }
+}
+
+const user : User ={
+    name:'rayhan',
+    address:{
+        city:'Jamalpur',
+        road:'Hashil',
+        presentAddress:'Dhaka'
+    }
+}
+
+
+
+const permenentAddress=user?.address?.permenentAddress??'no permenent address'
+console.log({permenentAddress});
+
+
+
 
 
 
